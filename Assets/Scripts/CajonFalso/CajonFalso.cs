70e15a6261f0c22e366b7f11bdef5655187bab21
SrTibii <marcluengoferra@gmail.com>
2026-02-19 17:02:29 +0100

    Cajon
    Programacion y animacion del cajon al pulsar "A"

---
+++ Assets/Scripts/CajonFalso/CajonFalso.cs
@@ -0,0 +1,51 @@
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CajonFalso : MonoBehaviour
+{
+
+    public Animator anim;
+    public string boolName = "isOpen";
+
+    public InputActionReference triggerAbrir;
+
+    bool isOpen = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        triggerAbrir.action.performed += OnTriggerPressedAbrir;
+        triggerAbrir.action.Enable();
+
+    }
+
+    void OnDisable()
+    {
+        triggerAbrir.action.performed -= OnTriggerPressedAbrir;
+        triggerAbrir.action.Disable();
+
+        
+    }
+
+    void OnTriggerPressedAbrir(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("armario");
+        isOpen = !isOpen;
+        anim.SetBool(boolName, isOpen);
+    }
+
+
+    
+}
