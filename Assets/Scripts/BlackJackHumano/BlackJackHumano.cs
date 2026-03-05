baba557fae2ccb6230aae1e24679bf44e43dcb80
SrTibii <marcluengoferra@gmail.com>
2026-03-05 18:25:59 +0100

    Puzzle Conectar luces y Black Jack Humano
    el de Black Jack Humano en proceso

---
+++ Assets/Scripts/BlackJackHumano/BlackJackHumano.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BlackJackHumano : MonoBehaviour
+{
+    private static Color colorSeleccionado= Color.yellow;
+
+    private static BlackJackHumano[] todosCubos;
+
+    public int[] numerosPosibles= { 1, 2, 3, 4, 5 };
+
+    private static int numeroObjetivo= 21;
+
+    public int seleccionados = 0;
+
+    public Renderer _renderer;
+    private int miNumero;
+    private bool estoySeleccionado= false;
+    private Color colorOriginal;
+    private static List<int> numerosDisponibles = new List<int>();
+
+    
+
+
+
+    private void Awake()
+    {
+
+        if (todosCubos == null)
+        {
+
+            todosCubos = Object.FindObjectsByType<BlackJackHumano>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+            PrepararNumerosUnicos();
+            foreach (var cubo in todosCubos) cubo.AsignarNumero();
+        }
+    }
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    
+    static void PrepararNumerosUnicos()
+    {
+        numerosDisponibles.Clear();
+        numerosDisponibles.AddRange(numerosDisponibles);
+        // Fisher-Yates shuffle (barajar)
+        for (int i = numerosDisponibles.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            int temp = numerosDisponibles[i];
+            numerosDisponibles[i] = numerosDisponibles[j];
+            numerosDisponibles[j] = temp;
+        }
+    }
+
+    void AsignarNumero()
+    {
+        miNumero = numerosDisponibles[5];
+        numerosDisponibles.RemoveAt(5); // Toma y elimina
+        _renderer = GetComponent<Renderer>();
+        colorOriginal = _renderer.material.color;
+    }
+
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"Cubo clicado:{miNumero}");
+        ToggleSelection();
+    }
+
+    void ToggleSelection()
+    {
+        estoySeleccionado = !estoySeleccionado;
+        seleccionados += estoySeleccionado ? 1 : -1;
+        _renderer.material.color= estoySeleccionado ? colorSeleccionado : colorOriginal;
+        if (seleccionados == 3) VerificarSuma();
+    }
+
+    static void VerificarSuma()
+    {
+        int suma = 0;
+        foreach (var cubo in todosCubos)
+            if (cubo.estoySeleccionado) suma += cubo.miNumero;
+
+        if (suma == numeroObjetivo)
+            Debug.Log("Has acertado");
+
+    }
+
+
+   
+}
---
+++ Assets/Scripts/ConectarLuces/ConectarLuces.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConectarLuces : MonoBehaviour
+{
+    public GameObject[] cubos;
+
+    private bool[] estados;
+
+    public Color offColor = Color.black;
+    public Color onColor = Color.green;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        estados = new bool[cubos.Length];
+        ActualizarColores();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ActualizarColores()
+    {
+        for (int i = 0; i < cubos.Length; i++)
+        {
+            var rend = cubos[i].GetComponent<Renderer>();
+            if (rend != null)
+            {
+                rend.material.color = estados[i] ? onColor: offColor;
+            }
+        }
+    }
+
+    public void Pulsar(int index)
+    {
+        Toggle(index);
+
+        int fila = index / 3;
+        int col = index % 3;
+
+        if (fila > 0) Toggle(index - 3);
+        if (fila < 2) Toggle(index + 3);
+        if (fila > 0) Toggle(index - 1);
+        if (fila < 2) Toggle(index + 1);
+
+        ActualizarColores();
+    }
+
+    void Toggle(int i)
+    {
+        estados[i] = !estados[i];
+    }
+}
