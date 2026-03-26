947056e5238f25322526f549ee6c9a24c4cde04c
sinnerbie <165671232+sinnerbie@users.noreply.github.com>
2026-03-26 16:14:56 +0100

    Sistema de inventario

---
+++ Assets/Scripts/Inventario/InventorySystem.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    [Header("Inventory")]
+    [SerializeField] private List<InventoryItem> _KeyStash = new List<InventoryItem>();
+    [SerializeField] private List<InventoryItem> _MiscStash = new List<InventoryItem>();
+    [Header("Define Item Positions")]
+    [SerializeField] private List<Transform> _KeyPositions = new List<Transform>();
+    [SerializeField] private List<Transform> _MiscPositions = new List<Transform>();
+
+    [Header("Asign Default Position")]
+    [SerializeField] private Vector3 defaultPos;
+    [SerializeField] private Quaternion defaultRot;
+
+    void Awake()
+    {
+        defaultPos = transform.position;
+        defaultRot = transform.rotation;
+    }
+
+    public void SnapItem(InventoryItem newIt)
+    {
+        newIt.stashed = true;
+        if (newIt.itemType == InventoryItem.Item.Key)
+        {
+            _KeyStash.Add(newIt);
+            for (int i = 0; i < _KeyPositions.Count; i++)
+                if (_KeyPositions[i].childCount == 0) 
+                {
+                    newIt.inventoryPos = _KeyPositions[i]; 
+                    return;
+                }
+        }
+        else if (newIt.itemType == InventoryItem.Item.Miscelaneous)
+        {
+            _MiscStash.Add(newIt);
+            for (int i = 0; i < _MiscPositions.Count; i++)
+                if (_MiscPositions[i].childCount == 0) 
+                {
+                    newIt.inventoryPos = _MiscPositions[i]; 
+                    return;
+                }
+        }
+    }
+
+    public void ReleaseItem(InventoryItem newIt)
+    {
+        newIt.stashed = false;
+        newIt.inventoryPos = null;
+        if (newIt.itemType == InventoryItem.Item.Key)
+            _KeyStash.Remove(newIt);
+        else if (newIt.itemType == InventoryItem.Item.Miscelaneous)
+            _MiscStash.Remove(newIt);
+    }
+
+    public void OnGrabInventory()
+    {
+        // Play animation
+        for (int i = 0; i < _KeyStash.Count; i++)
+            _KeyStash[i].gameObject.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>().enabled = true;
+        for (int i = 0; i < _MiscStash.Count; i++)
+            _MiscStash[i].gameObject.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>().enabled = true;
+    }
+
+    public void OnReleaseInventory()
+    {
+        for (int i = 0; i < _KeyStash.Count; i++)
+            _KeyStash[i].gameObject.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>().enabled = false;
+        for (int i = 0; i < _MiscStash.Count; i++)
+            _MiscStash[i].gameObject.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>().enabled = false;
+
+        transform.position = defaultPos;
+        transform.rotation = defaultRot;
+    }
+}
